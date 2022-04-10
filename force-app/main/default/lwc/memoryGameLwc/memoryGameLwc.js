ab635dd27a126f8c1a1786c92ccff6b9fe05da84
Sean <sean.raborg@gmail.com>
2022-04-10 09:18:56 -0700

    added/imported font-awesome as a static resource

--- force-app/main/default/lwc/memoryGameLwc/memoryGameLwc.js
+++ force-app/main/default/lwc/memoryGameLwc/memoryGameLwc.js
@@ -1,5 +1,16 @@
 import { LightningElement } from 'lwc';
-
+import {loadStyle} from 'lightning/platformResourceLoader'
+import fontawesome from '@salesforce/resourceUrl/fontawesome'
 export default class MemoryGameLwc extends LightningElement {
-    
+
+    isLibLoaded = false
+    renderedCallback() {
+        if(this.isLibLoaded === false) {
+            loadStyle(this, fontawesome+'/fontawesome/css/font-awesome.min.css').then(() => {
+                console.log("loaded succesfully")
+            }).catch(error => {console.error(error)})
+        
+            this.isLibLoaded = true
+        }
+    }
 }
